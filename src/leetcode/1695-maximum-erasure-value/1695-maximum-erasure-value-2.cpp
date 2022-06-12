991637bcae7a0ae410295230fe7d6544156c530c
Yi Byeongyong <ybyo@pm.me>
2022-06-12 20:44:46 +0900

    approach(leetcode): add new approach

---
+++ src/leetcode/1695-maximum-erasure-value/1695-maximum-erasure-value-2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int> &nums) {
+        int n = nums.size();
+        int curr_sum = 0;
+        int max_sum = 0;
+        int ldx = 0;
+        int rdx = 0;
+        array<bool, 100001> arr = {};
+        while (rdx < n) {
+            curr_sum += nums[rdx];
+            if (arr[nums[rdx]]) {
+                while (ldx < rdx && nums[ldx] != nums[rdx]) {
+                    curr_sum -= nums[ldx];
+                    arr[nums[ldx]] = false;
+                    ldx++;
+                }
+                curr_sum -= nums[ldx];
+                arr[nums[ldx]] = false;
+                ldx++;
+            }
+            arr[nums[rdx]] = true;
+            max_sum = max(max_sum, curr_sum);
+            rdx++;
+        }
+        return max_sum;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
