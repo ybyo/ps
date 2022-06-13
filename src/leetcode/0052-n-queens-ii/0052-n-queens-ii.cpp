3041bd15ee3572a8c45c7e7d0963aa47ae0ebf0e
Yi Byeongyong <ybyo@pm.me>
2022-06-13 12:07:39 +0900

    refactor: remove iteration macro
    remove iteration macro and change variable name and returning values to

--- src/leetcode/0052-n-queens-ii/0052-n-queens-ii.cpp
+++ src/leetcode/0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-// leetcode submit region begin(Prohibit modification and deletion)
+//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-#define REP(i, k, n) for (int i = k; i < n; i++)
-#define REPR(i, k, n) for (int i = k; i > n; i--)
 public:
     int solve(vector<bool> &col, vector<bool> &left_diag, vector<bool> &right_diag, int r) {
         int n = col.size();
         int cnt = 0;
-        if (r == n) return true;
-        REP(i, 0, n) {
+        if (r == n) {
+            return 1;
+        }
+        for (int i = 0; i < n; i++) {
             if (!col[i] && !left_diag[r + i] && !right_diag[r - i + n - 1]) {
                 col[i] = left_diag[r + i] = right_diag[r - i + n - 1] = true;
                 cnt += solve(col, left_diag, right_diag, r + 1);
@@ -22,8 +22,8 @@ public:
     }
 
     int totalNQueens(int n) {
-        vector<bool> col(n), diag(2 * n - 1), right_diag(2 * n - 1);
-        return solve(col, diag, right_diag, false);
+        vector<bool> col(n), left_diag(2 * n - 1), right_diag(2 * n - 1);
+        return solve(col, left_diag, right_diag, 0);
     }
 };
-// leetcode submit region end(Prohibit modification and deletion)
+//leetcode submit region end(Prohibit modification and deletion)
