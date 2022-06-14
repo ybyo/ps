4df3f776500ac3ef326cc387e0dead4ab42d0661
Yi Byeongyong <ybyo@pm.me>
2022-06-14 14:40:54 +0900

    sol(leetcode): add new solution

---
+++ src/leetcode/0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int minDistance(string &word1, string &word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector grid(n + 1, vector<int> (m + 1));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    grid[i][j] = grid[i - 1][j - 1] + 1;
+                } else {
+                    grid[i][j] = max(grid[i - 1][j], grid[i][j - 1]);
+                }
+            }
+        }
+        return n + m - 2 * grid[n][m];
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
