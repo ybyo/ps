57d935136593df065d52597e215cbfc668f56cc4
Yi Byeongyong <ybyo@pm.me>
2022-06-16 15:44:07 +0900

    sol(leetcode): add new solution

---
+++ src/leetcode/0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int expand(string s, int l, int r) {
+        while (l >= 0 && r < s.size() && s[l] == s[r]) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+
+    string longestPalindrome(string s) {
+        int l = 0;
+        int r = 0;
+        int len = 0;
+        for (int i = 0; i < s.size(); i++) {
+            int odd = expand(s, i, i);
+            int even = expand(s, i, i + 1);
+            len = max(odd, even);
+            if (len > r - l) {
+                l = i - (len - 1) / 2;
+                r = i + len / 2;
+            }
+        }
+        return s.substr(l, r - l + 1);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
