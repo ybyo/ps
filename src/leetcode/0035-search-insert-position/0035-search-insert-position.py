305e7bfdb86daec553936ef984587c37fd0789e7
Yi Byeongyong <ybyo@pm.me>
2022-06-18 12:51:38 +0900

    sol(leetcode): add new solution

---
+++ src/leetcode/0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,16 @@
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l
+
+# leetcode submit region end(Prohibit modification and deletion)
