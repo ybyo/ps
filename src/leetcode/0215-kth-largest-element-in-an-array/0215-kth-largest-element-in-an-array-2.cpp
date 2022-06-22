f98526b0d0608b8adfea2f1c5bb035a2f94b7a39
Yi <ybyo@yibyeongyong.com>
2022-06-22 18:04:16 +0900

    sol(leetcode): add new solution

---
+++ src/leetcode/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array-2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int i = 0; i < k; ++i) {
+            pq.emplace(nums[i]);
+        }
+        for (int i = k; i < nums.size(); ++i) {
+            if (pq.top() <= nums[i]) {
+                pq.pop();
+                pq.emplace(nums[i]);
+            }
+        }
+        return pq.top();
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
