0a20b8abe70990f9156f31eb791d62d99069c727
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-06-25 14:12:40 +0900

    sol(leetcode): add new solution
    Signed-off-by: Yi Byeongyong <ybyo@yibyeongyong.com>

---
+++ src/leetcode/1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    bool isPossible(vector<int> &target) {
+        priority_queue<int> pq(target.begin(), target.end());
+        long long sum = 0;
+        for (auto &num: target) {
+            sum += num;
+        }
+        while (pq.top() != 1) {
+            sum -= pq.top();
+            if (sum == 0 || sum >= pq.top()) {
+                return false;
+            }
+            int min_val = pq.top() % sum;
+            if (sum != 1 && min_val == 0) {
+                return false;
+            }
+            pq.pop();
+            pq.emplace(min_val);
+            sum += min_val;
+        }
+        return true;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
