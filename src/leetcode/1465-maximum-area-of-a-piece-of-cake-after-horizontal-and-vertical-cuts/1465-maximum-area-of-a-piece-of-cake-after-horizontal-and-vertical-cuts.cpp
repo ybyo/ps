11e76859110049df88d802c7236a6655bfdcc84e
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-07-02 18:52:02 +0900

    style(leetcode): fix code expression
    Signed-off-by: Yi Byeongyong <ybyo@yibyeongyong.com>

--- src/leetcode/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ src/leetcode/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -5,7 +5,7 @@ using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
-    const long long int m = pow(10, 9) + 7;
+    long long m = pow(10, 9) + 7;
     int maxArea(int h, int w, vector<int> &horizontalCuts, vector<int> &verticalCuts) {
         sort(horizontalCuts.begin(), horizontalCuts.end());
         sort(verticalCuts.begin(), verticalCuts.end());
@@ -21,9 +21,9 @@ public:
             wvec.emplace_back(verticalCuts[i] - verticalCuts[i - 1]);
         }
         wvec.emplace_back(w - verticalCuts.back());
-        long long int a = *max_element(hvec.begin(), hvec.end());
-        long long int b = *max_element(wvec.begin(), wvec.end());
-        return (a % m * b % m);
+        long long a = *max_element(hvec.begin(), hvec.end());
+        long long b = *max_element(wvec.begin(), wvec.end());
+        return a % m * b % m;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
