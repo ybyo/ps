4707a6ec8e43a704d7eb541fa2fa6b78f62c2e17
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-07-09 17:58:03 +0900

    sol(leetcode): add new solutions
    Signed-off-by: Yi Byeongyong <ybyo@yibyeongyong.com>

---
+++ src/leetcode/0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode *root) {
+        if (root == nullptr) return {};
+        vector<vector<int>> ans;
+        queue<TreeNode *> q;
+        q.emplace(root);
+        int sz;
+        while (!q.empty()) {
+            sz = q.size();
+            TreeNode *tmp;
+            vector<int> vec;
+            for (int i = 0; i < sz; ++i) {
+                tmp = q.front();
+                q.pop();
+                vec.emplace_back(tmp->val);
+                if(tmp->left) q.emplace(tmp->left);
+                if(tmp->right) q.emplace(tmp->right);
+            }
+            ans.emplace_back(vec);
+        }
+        return ans;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
