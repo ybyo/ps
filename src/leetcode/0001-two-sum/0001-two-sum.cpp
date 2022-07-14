075d2844cf6823a4b0bf45814b378ef30f973ecf
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-07-14 17:09:34 +0900

    refactor(leetcode): remove macros
    Signed-off-by: Yi Byeongyong <ybyo@yibyeongyong.com>

--- src/leetcode/0001-two-sum/0001-two-sum.cpp
+++ src/leetcode/0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,9 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-#define REP(i, k, n) for (int i = k; i < n; i++)
-#define REPR(i, k, n) for (int i = k; i > n; i--)
 public:
     vector<int> twoSum(vector<int> &nums, int target) {
         unordered_map<int, int> umap;
         int n = nums.size();
-        REP(i, 0, n) {
+        for (int i = 0; i < n; ++i) {
             if (umap.find(target - nums[i]) != umap.end()) {
                 return {i, umap[target - nums[i]]};
             } else umap[nums[i]] = i;
@@ -18,4 +11,3 @@ public:
         return {};
     }
 };
-//leetcode submit region end(Prohibit modification and deletion)
