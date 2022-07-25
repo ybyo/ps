af2d144de44f14d3ea4d4fe761a2bd4aebc0cac8
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-07-25 20:00:12 +0900

    approach(leetcode): add new approach

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
@@ -1,3 +1,17 @@
 class Solution:
     def fillCups(self, amount: List[int]) -> int:
+        ans = 0
+        amount.sort(reverse=True)
+        while amount[0] != 0:
+            ans += 1
+            for i in range(2):
+                if amount[i] != 0:
+                    amount[i] -= 1
+            amount.sort(reverse=True)
+        
+        return ans
+
+
+class Solution:
+    def fillCups(self, amount: List[int]) -> int:
         return max(max(amount), (sum(amount) + 1)//2)
