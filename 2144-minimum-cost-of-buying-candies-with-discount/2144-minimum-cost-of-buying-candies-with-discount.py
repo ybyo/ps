7a628fbc0e28e1bee71a81e12919e7a16e8a1550
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-07-28 09:37:45 +0900

    approach(leetcode): add new approach

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -1,4 +1,16 @@
 class Solution:
     def minimumCost(self, cost: List[int]) -> int:
+        ans = cnt = 0
+        cost.sort(reverse=True)
+        for i in cost:
+            cnt += 1
+            if cnt % 3 == 0:
+                continue
+            ans += i
+        return ans
+
+
+class Solution:
+    def minimumCost(self, cost: List[int]) -> int:
         cost.sort(reverse=True)
         return sum(cost) - sum(cost[2::3])
