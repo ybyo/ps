aaa8774ee582d5c7291c19f9b06555c7680c4986
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-08-01 11:48:55 +0900

    Time: 54 ms (40.82%), Space: 13.8 MB (94.92%) - LeetHub

--- 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.py
+++ 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.py
@@ -4,6 +4,7 @@ class Solution:
         curr = 'a'
         for c in word:
             gap = abs(ord(curr) - ord(c))
+            # gap = (ord(curr) - ord(c)) % 26
             ans += min(gap, 26 - gap)
             curr = c
         return ans
