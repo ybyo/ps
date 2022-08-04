f1353dc1924eb4fc8c319f32e02855e3475141bc
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-08-04 10:08:52 +0900

    Time: 36 ms (99.46%), Space: 14 MB (94.50%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,6 +1,6 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for i in set(ransomNote):
-            if ransomNote.count(i) > magazine.count(i):
+        for c in set(ransomNote):
+            if ransomNote.count(c) > magazine.count(c):
                 return False
         return True
