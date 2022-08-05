946b8722d6120841db348a52b7d9702ac09fe505
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-08-05 23:38:33 +0900

    Time: 233 ms (68.17%), Space: 14.4 MB (40.21%) - LeetHub

--- 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.py
@@ -1,7 +1,7 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
         ans = []
-        min_idx = inf
+        min_idx = 2 * 10**3
         for menu in set(list1).intersection(set(list2)):
             idx_sum = list1.index(menu) + list2.index(menu)
             if min_idx > idx_sum:
