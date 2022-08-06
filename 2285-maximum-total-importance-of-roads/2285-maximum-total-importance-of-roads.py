4703282d52e26b4cd655c2607b60c43d3d09d951
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-08-06 13:26:53 +0900

    Time: 2645 ms (44.40%), Space: 42.5 MB (18.99%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
@@ -1,14 +1,12 @@
-import heapq
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
+        adj_list = {i: [] for i in range(n)}
         ans = 0
-        values = [0] * n
         for a, b in roads:
-            values[a] += 1
-            values[b] += 1
-        heapq.heapify(values)
-        w = 1
-        while values:
-            ans += heapq.heappop(values) * w
-            w += 1
+            adj_list[a].append(b)
+            adj_list[b].append(a)
+        adj_list = dict(sorted(adj_list.items(), key=lambda x: len(x[1]), reverse=True))
+        for k, v in adj_list.items():
+            ans += n * len(v)
+            n -= 1
         return ans
