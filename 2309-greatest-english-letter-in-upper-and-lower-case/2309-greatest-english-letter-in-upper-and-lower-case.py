b347bb142b825761ea1efbec000fc596b410e7a7
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-08-10 22:18:37 +0900

    Time: 35 ms (96.18%), Space: 13.7 MB (98.28%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
@@ -1,12 +1,7 @@
 class Solution:
     def greatestLetter(self, s: str) -> str:
-        ls = list(s.lower())
-        ls.sort(reverse = True)
-        
-        for c in ls:
-            if ls.count(c) >= 2:
-                if s.find(c) != -1 and s.find(c.upper()) != -1:
-                    return c.upper()
-        
+        ss = set(s)
+        for c in 'ZYXWVUTSRQPONMLKJIHGFEDCBA':
+            if c.lower() in ss and c in ss:
+                return c
         return ''
-        
