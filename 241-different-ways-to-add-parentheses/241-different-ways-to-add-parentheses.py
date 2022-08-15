8faa0e5fad66b6b56340eb801f2cf83cc222a5b2
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-08-15 22:27:26 +0900

    Time: 37 ms (92.47%), Space: 14 MB (52.57%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.py
@@ -1,24 +1,21 @@
 class Solution:
-    def solve(self, s: str, ops: dict) -> List[int]:
-        if s.isdigit():
-            return [int(s)]
-
-        ans = []
-
-        for i in range(len(s)):
-            if s[i] in ops:
-                lt = self.solve(s[:i], ops)
-                rt = self.solve(s[i + 1:], ops)
-                for a, b in product(lt, rt):
-                    ans.append(ops[s[i]](a, b))
-
-        return ans
-
     def diffWaysToCompute(self, s: str) -> List[int]:
+        ans = []
+        
         ops = {
             '+': lambda x, y: x + y,
             '-': lambda x, y: x - y,
             '*': lambda x, y: x * y,
         }
+        
+        if s.isdigit():
+            return [int(s)]
 
-        return self.solve(s, ops)
+        for i in range(len(s)):
+            if s[i] in ops:
+                lt = self.diffWaysToCompute(s[:i])
+                rt = self.diffWaysToCompute(s[i + 1:])
+                for a, b in product(lt, rt):
+                    ans.append(ops[s[i]](a, b))
+
+        return ans
