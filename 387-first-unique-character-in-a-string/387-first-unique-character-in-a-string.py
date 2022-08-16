d681fa961f54c8d7ff6dc0962ca6f1cd1350d7fe
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-08-16 16:36:04 +0900

    Time: 44 ms (99.99%), Space: 14 MB (96.20%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        abc = "abcdefghijklmnopqrstuvwxyz"
+        ans = 10**5
+        
+        for c in abc:
+            idx = s.find(c);
+            if idx != -1 and idx == s.rfind(c):
+                ans = min(ans, idx)
+
+        return ans if ans < 10**5 else -1
