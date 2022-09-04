d3e52de79160b6f7cbc1526de4841dd4ed09bbdb
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-09-04 09:31:03 +0900

    Time: 162 ms (80.83%), Space: 17.1 MB (5.17%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         """
-        Category: Array, Bit Manipulation
+        Topics: Array, Bit Manipulation
         """
         if len(nums) == 1:
             return nums[0]
