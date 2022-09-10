e778b2ae269396de01b61ad49359060e86b41561
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-09-11 08:42:59 +0900

    Time: 279 ms (34.31%), Space: 13.8 MB (94.99%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -8,5 +8,5 @@ class Solution:
             for i in range(1, k + 1):
                 dp[i][0] = min(dp[i][0], p - dp[i - 1][1])
                 dp[i][1] = max(dp[i][1], p - dp[i][0])
-
+                
         return dp[k][1]
