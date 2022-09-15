a0d563fa0cf41bbed4835342e544153a1d3f840e
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-09-15 23:12:42 +0900

    Time: 94 ms (5.53%), Space: 14.1 MB (22.83%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def bt(ss, vstd):
+            if len(ss) == len(nums):
+                ans.append(ss[:])
+                return
+
+            for i in range(len(nums)):
+                if vstd[i]:
+                    continue
+                ss.append(nums[i])
+                vstd[i] = True
+                bt(ss, vstd)
+                ss.pop()
+                vstd[i] = False
+
+        ans = []
+        bt([], [False] * len(nums))
+        return ans
