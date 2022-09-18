0c0b92995f81504babadfb6bbfbc6d90d032f2a1
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-09-18 09:51:36 +0900

    Time: 198 ms (85.88%), Space: 17.3 MB (88.44%) - LeetHub

--- 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.py
+++ 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.py
@@ -6,33 +6,18 @@
 #         self.right = right
 class Solution:
     memo = {0: [], 1: [TreeNode()]}
+
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
-        def deepCopy(node):
-            if not node:
-                return None
-            new_node = TreeNode()
-            new_node.left = deepCopy(node.left)
-            new_node.right = deepCopy(node.right)
-            return new_node
-        
-        if n % 2 == 0:
-            return []
-        if n == 1:
-            return [TreeNode()]
-        
         if n not in Solution.memo:
             ans = []
             for x in range(n):
                 y = n - 1 - x
-                lb = self.allPossibleFBT(x)
-                rb = self.allPossibleFBT(y)
-                for lcnt, l in enumerate(lb, 1):
-                    for rcnt, r in enumerate(rb, 1):
+                for l in self.allPossibleFBT(x):
+                    for r in self.allPossibleFBT(y):
                         node = TreeNode()
-                        node.left = deepCopy(l) if rcnt < len(rb) else l
-                        node.right = deepCopy(r) if lcnt < len(lb) else r
+                        node.left = l
+                        node.right = r
                         ans.append(node)
             Solution.memo[n] = ans
-        
-        
+
         return Solution.memo[n]
