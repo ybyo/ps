0129da3162ba01b4f1861ebeec17e1ebdb535200
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-09-25 10:13:31 +0900

    Time: 662 ms (99.82%), Space: 28.7 MB (10.44%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -7,4 +7,15 @@ class Solution:
 
         return max(dp)
                 
-            
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = 10**4 * -1
+        curr_sum = 0
+        for n in nums:
+            curr_sum = n + curr_sum
+            if max_sum < curr_sum:
+                max_sum = curr_sum
+            if curr_sum <= 0:
+                curr_sum = 0 
+
+        return max_sum
