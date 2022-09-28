a9626adccd5c1bc8e5e453abdaa3f4176c397f9d
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-09-28 23:38:39 +0900

    chore(leetcode): update file path
    Signed-off-by: Yi Byeongyong <ybyo@yibyeongyong.com>

--- src/leetcode/0200-number-of-islands/200-number-of-islands.py
+++ src/leetcode/0200-number-of-islands/200-number-of-islands.py
@@ -13,3 +13,29 @@ class Solution:
             rr, cc = r + a, c + b
             if 0 <= rr < len(grid) and 0 <= cc < len(grid[0]) and grid[rr][cc] == '1':
                 self.dfs(grid, rr, cc)
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ans = 0
+        m, n = len(grid), len(grid[0])
+
+        def dfs(y, x):
+            grid[y][x] = '0'
+            for a, b in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nr = y + a
+                nc = x + b
+                if check(nr, nc) and grid[nr][nc] == '1':
+                    dfs(nr, nc)
+
+        def check(rr, cc):
+            if 0 <= rr < m and 0 <= cc < n:
+                return True
+            return False
+
+        for r, c in product(range(m), range(n)):
+            if grid[r][c] == '1':
+                dfs(r, c)
+                ans += 1
+
+        return ans
